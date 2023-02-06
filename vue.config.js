ac1f70b6de86a2a0ee26d3951a6584d6961808b9
g1372 <1372137936@qq.com>
2023-02-06 22:11:55 +0800

    配置未做任何更改

--- vue.config.js
+++ vue.config.js
@@ -1,24 +1,24 @@
 const path = require('path')
 module.exports = {
-    lintOnSave: false, // 修改成false 就是不检查了
-    pages: {
-        index: {
-            // 修改项目入口文件
-            entry: path.resolve(__dirname, 'website/examples/main.js'),
-            template: path.resolve(__dirname, 'website/public/index.html'),
-            filename: 'index.html'
-        }
-    },
-    // 扩展webpack配置,使webpages加入编译
-    chainWebpack: config => {
-        config.module
-            .rule('js')
-            .include.add(path.resolve(__dirname, 'packages'))
-            .end()
-            .use('babel')
-            .loader('babel-loader')
-            .tap(options => {
-                return options
-            })
+  lintOnSave: false, // 修改成false 就是不检查了
+  pages: {
+    index: {
+      // 修改项目入口文件
+      entry: path.resolve(__dirname, 'website/examples/main.js'),
+      template: path.resolve(__dirname, 'website/public/index.html'),
+      filename: 'index.html'
     }
+  },
+  // 扩展webpack配置,使webpages加入编译
+  chainWebpack: config => {
+    config.module
+      .rule('js')
+      .include.add(path.resolve(__dirname, 'packages'))
+      .end()
+      .use('babel')
+      .loader('babel-loader')
+      .tap(options => {
+        return options
+      });
+  }
 }
