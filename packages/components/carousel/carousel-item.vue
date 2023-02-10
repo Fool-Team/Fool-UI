a2e614a14b51380859fddbec70ef31ef0bbff671
g1372 <1372137936@qq.com>
2023-02-10 12:20:24 +0800

    创建 carousel 子组件

---
+++ packages/components/carousel/carousel-item.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="fool-carousel__item">
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'foolCarouselItem'
+}
+</script>
+
+<style lang="scss" scoped>
+@use '../../theme-thalk/carousel-item.scss' as *;
+</style>
