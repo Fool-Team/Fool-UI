56b9d2311ef146159ba9f57418570a3324740493
g1372 <1372137936@qq.com>
2023-02-11 23:21:06 +0800

    无修改

--- deploy.sh
+++ deploy.sh
@@ -17,9 +17,10 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
-git push -f git@github.com:Fool-Team/docs.git master:gh-pages
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+git push -f git@github.com:Gg1372/Fool-UI/tree/dev/docs.git master:dev
 
 cd -
