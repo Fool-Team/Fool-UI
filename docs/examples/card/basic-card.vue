90ea8b660b11ecff49677faadcd93588eae9ed31
g1372 <1372137936@qq.com>
2023-02-12 21:33:23 +0800

    创建 card 文章相关组件

---
+++ docs/examples/card/basic-card.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="basic-card">
+    <fool-card shadow="always">
+      <template #header>
+        <div class="card-header">
+          <div class="card-name">Card name</div>
+          <fool-button :round="true">Operation button</fool-button>
+        </div>
+      </template>
+      <div v-for="i in 4" :key="i" class="card-body-item">List Item {{i}}</div>
+    </fool-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'basic-card'
+}
+</script>
+
+<style lang="scss" scoped>
+.card-header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.card-body-item {
+  margin-bottom: 18px;
+}
+</style>
---
+++ docs/examples/card/shadow-card.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="basic-card">
+    <fool-row :gutter="20">
+      <fool-col :span="8">
+        <fool-card shadow="always">Always</fool-card>
+      </fool-col>
+      <fool-col :span="8">
+        <fool-card shadow="hover">Hover</fool-card>
+      </fool-col>
+      <fool-col :span="8">
+        <fool-card>Never</fool-card>
+      </fool-col>
+    </fool-row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'shadow-card'
+}
+</script>
+
+<style lang="scss" scoped>
+.card-body-item {
+  padding: 18px 0;
+  margin-bottom: 18px;
+}
+</style>
