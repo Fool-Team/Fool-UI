9d13538890122f206cd40feb6c414a5ff22b0fe5
彭文豪 <2279265610@qq.com>
2023-02-23 12:22:25 +0800

    完成switch

---
+++ docs/examples/switch/switch.vue
@@ -0,0 +1,21 @@
+<template>
+  <fool-switch v-model="active" name="username"></fool-switch>
+</template>
+
+<script>
+export default {
+  name: 'switch-pwh',
+  data() {
+    return {
+      username: '123456',
+      active: false
+    }
+  }
+}
+</script>
+
+<style>
+.fool-row {
+  margin-bottom: 20px;
+}
+</style>
